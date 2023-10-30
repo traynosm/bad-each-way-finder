573d7dd42d50a07e8edca96337e1ef184f656866
Susan Traynor <traynosm@gmail.com>
2023-10-30 09:29:56 +0000

    rescaffolded
    Remove the identity platform. Clicked incorrectly on creation. Using sql server

--- src/bad-each-way-finder/bad-each-way-finder/Program.cs
+++ src/bad-each-way-finder/bad-each-way-finder/Program.cs
@@ -1,13 +1,6 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using bad_each_way_finder.Areas.Identity.Data;
-
 namespace bad_each_way_finder
 {
     public class Program
@@ -20,23 +13,14 @@ namespace bad_each_way_finder
                 throw new InvalidOperationException("Connection string 'BadEachWayFinder' not found.");
 
             builder.Services.AddDbContext<BadEachWayFinderContext>(options => 
-                options.UseSqlite(connectionString));
+                options.UseSqlServer(connectionString));
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => 
                 options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<BadEachWayFinderContext>();
 
             // Add services to the container.
-            builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
-
-            builder.Services.AddAuthorization(options =>
-            {
-                // By default, all incoming requests will be authorized according to the default policy.
-                options.FallbackPolicy = options.DefaultPolicy;
-            });
-            builder.Services.AddRazorPages()
-                .AddMicrosoftIdentityUI();
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
@@ -56,7 +40,6 @@ namespace bad_each_way_finder
             app.UseAuthorization();
 
             app.MapRazorPages();
-            app.MapControllers();
 
             app.Run();
         }
